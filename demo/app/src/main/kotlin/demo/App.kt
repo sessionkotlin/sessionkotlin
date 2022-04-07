8569162c093a86224ba4b20e86fcefd0516df238
David Amorim <dmaa.costa@campus.fct.unl.pt>
2022-04-07 02:12:45 +0100

    Class visibility

--- demo/app/src/main/kotlin/demo/App.kt
+++ demo/app/src/main/kotlin/demo/App.kt
@@ -1,8 +1,7 @@
 package demo
 
 
-import Proto1A1
-import Proto1A2
+
 import org.david.sessionkotlin_lib.api.SKBuffer
 import org.david.sessionkotlin_lib.dsl.*
 import java.io.File
@@ -20,12 +19,12 @@ fun main() {
         goto(t1)
     }
 
-    val buf = SKBuffer<Int>()
+    Proto1A1()
+        .sendToB(10)
+        .receiveFromB(SKBuffer())
+        .receiveFromB(SKBuffer())
+        .receiveFromB(SKBuffer())
 
-//    Proto1A1()
-//        .sendToB(10)
-//        .receiveFromB(buf)
-//
-//    Proto1A2()
+
 
 }
--- sessionkotlin/lib/src/main/kotlin/org/david/sessionkotlin_lib/dsl/RootEnv.kt
+++ sessionkotlin/lib/src/main/kotlin/org/david/sessionkotlin_lib/dsl/RootEnv.kt
@@ -1,11 +1,6 @@
 package org.david.sessionkotlin_lib.dsl
 
-import com.squareup.kotlinpoet.ClassName
-import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
-import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
-import com.squareup.kotlinpoet.TypeSpec
-import org.david.sessionkotlin_lib.api.SKBuffer
+import com.squareup.kotlinpoet.*
 import org.david.sessionkotlin_lib.dsl.types.*
 import java.io.File
 
@@ -46,56 +41,83 @@ public class RootEnv(private val name: String) : GlobalEnv(emptySet(), emptySet(
         fileSpecBuilder.build().writeTo(directory)
     }
 
+    private data class InterfaceClassPair(val interfaceClassName: ClassName, val className: ClassName)
+
     private fun genLocals(
         fileSpecBuilder: FileSpec.Builder,
         l: LocalType,
         r: Role,
         stateCount: Int,
-        recMap: Map<RecursionTag, ClassName>,
+        recMap: Map<RecursionTag, InterfaceClassPair>,
         tag: RecursionTag? = null,
-    ): ClassName {
+    ): InterfaceClassPair {
+
+        val interfaceName = ClassName("", "$name$r${stateCount}Interface")
         val className = ClassName("", "$name$r$stateCount")
 
-        val classBuilder = TypeSpec.classBuilder(className)
+        val interfaceBuilder = TypeSpec.interfaceBuilder(interfaceName)
+        val classBuilder = TypeSpec
+            .classBuilder(className)
+            .addSuperinterface(interfaceName)
+            .addModifiers(KModifier.PRIVATE)
 
         return when (l) {
-            LocalTypeEnd -> endClassName
+            LocalTypeEnd -> InterfaceClassPair(endClassName, endClassName)
             is LocalTypeSend -> {
-                println(tag)
-                val m = if (tag != null) recMap.plus(Pair(tag, className)) else recMap
-                val nextClassName = genLocals(fileSpecBuilder, l.cont, r, stateCount + 1, m)
-                val f = FunSpec.builder("sendTo${l.to}")
+                val m = if (tag != null) recMap.plus(Pair(tag, InterfaceClassPair(interfaceName, className))) else recMap
+                val nextInterfaceClassPair = genLocals(fileSpecBuilder, l.cont, r, stateCount + 1, m)
+                val methodName = "sendTo${l.to}"
+
+                val abstractFunction = FunSpec.builder(methodName)
                     .addParameter("arg", l.type.kotlin)
-                    .returns(nextClassName)
+                    .returns(nextInterfaceClassPair.interfaceClassName)
+                    .addModifiers(KModifier.ABSTRACT)
+
+                val function = FunSpec.builder(methodName)
+                    .addParameter("arg", l.type.kotlin)
+                    .addModifiers(KModifier.OVERRIDE)
+                    .returns(nextInterfaceClassPair.interfaceClassName)
                     .let {
                         it.addStatement("println(\"Send to ${l.to}\")")
-                        it.addStatement("return (${nextClassName.constructorReference()})()")
+                        it.addStatement("return (${nextInterfaceClassPair.className.constructorReference()})()")
                         it
                     }
-                classBuilder.addFunction(f.build())
+                interfaceBuilder.addFunction(abstractFunction.build())
+                classBuilder.addFunction(function.build())
+
+                fileSpecBuilder.addType(interfaceBuilder.build())
                 fileSpecBuilder.addType(classBuilder.build())
 
-                className
+                InterfaceClassPair(interfaceName, className)
             }
             is LocalTypeReceive -> {
-                println(tag)
+                val m = if (tag != null) recMap.plus(Pair(tag, InterfaceClassPair(interfaceName, className))) else recMap
+                val nextInterfaceClassPair = genLocals(fileSpecBuilder, l.cont, r, stateCount + 1, m)
+                val methodName = "receiveFrom${l.from}"
 
-                val m = if (tag != null) recMap.plus(Pair(tag, className)) else recMap
-                val nextClassName = genLocals(fileSpecBuilder, l.cont, r, stateCount + 1, m)
+                val abstractFunction = FunSpec.builder(methodName)
+                    .addParameter("arg", l.type.kotlin)
+                    .returns(nextInterfaceClassPair.interfaceClassName)
+                    .addModifiers(KModifier.ABSTRACT)
 
-                val f = FunSpec.builder("receiveFrom${l.from}")
-                    .addParameter("buf", SKBuffer::class.parameterizedBy(l.type.kotlin))
-                    .returns(nextClassName)
+                val function = FunSpec.builder(methodName)
+                    .addParameter("arg", l.type.kotlin)
+                    .returns(nextInterfaceClassPair.interfaceClassName)
+                    .addModifiers(KModifier.OVERRIDE)
                     .let {
                         it.addStatement("println(\"Receive from ${l.from}\")")
-                        it.addStatement("return (${nextClassName.constructorReference()})()")
+                        it.addStatement("return (${nextInterfaceClassPair.className.constructorReference()})()")
                         it
                     }
-                classBuilder.addFunction(f.build())
-                val directory = File("build/generated/sessionkotlin/main/kotlin")
+
+                interfaceBuilder.addFunction(abstractFunction.build())
+                classBuilder.addFunction(function.build())
+
+                fileSpecBuilder.addType(interfaceBuilder.build())
                 fileSpecBuilder.addType(classBuilder.build())
+
                 fileSpecBuilder.build().writeTo(directory)
-                className
+                InterfaceClassPair(interfaceName, className)
             }
             is LocalTypeRecursion -> recMap.getValue(l.tag)
             is LocalTypeRecursionDefinition -> genLocals(fileSpecBuilder, l.cont, r, stateCount, recMap, l.tag)
